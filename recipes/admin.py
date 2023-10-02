0fab94ed2f7f28bdfd0ef52061c7dba0aa55037c
daniesupratmanto13 <daniesupratmanto@gmail.com>
2023-10-02 09:14:31 +0800

    add models on recipes app

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# models
+from .models import Recipe, RecipeCategory, Like
+
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(RecipeCategory)
+admin.site.register(Like)
--- recipes/models.py
+++ recipes/models.py
@@ -1,15 +1,64 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
-class RecipeCategory():
-    pass
+class RecipeCategory(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Category Name"))
+    slug = models.SlugField(
+        max_length=120, null=False, blank=True, unique=True, editable=False
+    )
+
+    class Meta:
+        verbose_name = _('Recipe Category')
+        verbose_name_plural = _('Recipe Categories')
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
-class Recipe():
-    pass
+def get_default_recipe_category():
+
+    return RecipeCategory.objects.get_or_create(name='Others')[0]
 
 
-class Like():
-    pass
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User, related_name="recipes", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        RecipeCategory, on_delete=models.SET(get_default_recipe_category))
+    picture = models.ImageField(upload_to="recipes/")
+    title = models.CharField(max_length=200)
+    descripsion = models.CharField(_('description'), max_length=200)
+    cook_time = models.TimeField()
+    ingredients = models.TextField()
+    procedure = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    value = models.CharField(
+        choices=LIKE_CHOICES, default='Like', max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} {self.value} {self.recipe}'
