4e6ec640a0b6f5df3c326057ebc6ac96b3759cb6
daniesupratmanto13 <daniesupratmanto@gmail.com>
2023-10-04 23:02:12 +0800

    recipe category serializer

---
+++ recipes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+# models
+from .models import RecipeCategory
+
+
+class RecipeCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeCategory
+        fields = ['id', 'name', 'slug']
