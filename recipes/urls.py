3d2eacaf35663172f93b48e225a842472e333963
daniesupratmanto13 <daniesupratmanto@gmail.com>
2023-10-24 10:59:29 +0800

    urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,6 +8,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.RecipeListAPI.as_view(), name='list'),
     path('create/', views.RecipeCreateAPI.as_view(), name='create'),
+    path('like/<int:pk>/', views.RecipeLikeAPI.as_view(), name='like'),
     path('detail/<int:pk>/', views.RecipeDetailAPI.as_view(), name='detail'),
     path('update/<int:pk>/', views.RecipeUpdateDestroyAPI.as_view(), name='update'),
     path('delete/<int:pk>/', views.RecipeUpdateDestroyAPI.as_view(), name='delete'),
